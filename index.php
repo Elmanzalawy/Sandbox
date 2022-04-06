77cce6494373f936e2a95f6e3ee1232c2c87e7c5
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-06 22:28:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+/*
+function recursiveCountdown($n){
+    echo "$n".PHP_EOL;
+    
+    //define exit condition
+    if($n > 0){
+        recursiveCountdown($n - 1);
+    }
 
+}
+
+recursiveCountdown(10);
+*/
+
+
+
+/*
+function sumRangeRecursive($n, $total = 0)
+{
+    //define exit condition
+    if($n <= 0){
+        return $total;
+    }
+
+    return sumRangeRecursive($n - 1, $total + $n);
+}
+
+echo sumRangeRecursive(3);
+*/
 ?>
