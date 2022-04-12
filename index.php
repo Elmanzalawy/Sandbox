6e102cfaad4b28f83372eaa306a50e8ebdf41126
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-12 20:04:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
-?>
+class Bird{
+
+    /**
+     * Code executed when a class object is constructed, typically used to initialize the properties of an object
+     */
+    public function __construct()
+    {
+        echo "New Bird! 🐣".PHP_EOL;
+    }
+
+    /**
+     * Code executed when a class object is destroyed
+     */
+    public function __destruct()
+    {
+        echo "RIP Bird 💀 :(".PHP_EOL;
+    }
+}
+
+$bird = new Bird;
