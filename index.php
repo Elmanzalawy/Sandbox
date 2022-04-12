91d88192a5aac5e2aaf7a043f0edc5865e240a17
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-12 19:48:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php
 
-?>
+
+abstract class Animal
+{
+    abstract function move();
+}
+
+class Cat extends Animal{
+    function move()
+    {
+        echo "Cat runs".PHP_EOL;
+    }
+}
+
+class Bird extends Animal{
+    function move()
+    {
+        echo "Bird flies".PHP_EOL;
+    }
+}
+
+class Fish extends Animal{
+    function move()
+    {
+        echo "Fish swim".PHP_EOL;
+    }
+}
+
+$cat = new Cat;
+$bird = new Bird;
+$fish = new Fish;
+
+$cat->move();
+$bird->move();
+$fish->move();
