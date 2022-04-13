dda4fe9d58a8f14c75ca34d99294c0b8f976cc66
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-13 12:58:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,49 @@
 <?php
 
-?>
+class BankAccount
+{
+    private float $balance;
+
+    public function __construct($balance = 0)
+    {
+        $this->balance = $balance;
+    }
+
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    public function deposit($amount)
+    {
+        if ($amount > 0) {
+            $this->balance += $amount;
+        }
+
+        return $this;
+    }
+}
+
+class SavingAccount extends BankAccount
+{
+
+    private float $interestRate;
+
+    public function setInterestRate($rate)
+    {
+        $this->interestRate = $rate;
+        return $this;
+    }
+
+    public function addInterest()
+    {
+        $interest = $this->interestRate * $this->getBalance();
+
+        $this->deposit($interest);
+        return $this;
+    }
+}
+
+$savingAccount = new SavingAccount(100);
+$savingAccount->setInterestRate(0.02)->addInterest()->getBalance();
+echo $savingAccount->getBalance();
