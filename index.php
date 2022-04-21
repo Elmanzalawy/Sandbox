72d730c52d2134b4e998dd96b904498dcc329213
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2022-04-21 03:21:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+trait Logger
+{
+    public function log($msg)
+    {
+        echo date('Y-m-d H:i:s'). ' - ' . '('. __CLASS__.'): '. $msg . PHP_EOL;
+    }
+}
+
+
+class BankAccount
+{
+    use Logger;
+
+    private $accountNumber;
+
+    public function __construct($accountNumber)
+    {
+        $this->accountNumber = $accountNumber;
+        $this->log("A new $accountNumber bank account has been created.");
+    }
+}
+
+$bankAccount = new BankAccount("1234565");
