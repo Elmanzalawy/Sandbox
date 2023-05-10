7e6a9c4bc2e7a2019539b72f892bf8f034ac1dff
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2023-05-11 00:43:41 +0300

    abstract-classes

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
 <?php
+
+abstract class Car
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    abstract public function description();
+    public function move(): string
+    {
+        return "$this->name is moving..";
+    }
+}
+
+// Child classes
+class Audi extends Car
+{
+    public function description(): string
+    {
+        return "Choose German quality! I'm an $this->name!";
+    }
+}
+
+class Volvo extends Car
+{
+    public function description(): string
+    {
+        return "Proud to be Swedish! I'm a $this->name!";
+    }
+}
+
+
+// Create objects from the child classes
+$audi = new audi("Audi");
+echo $audi->description();
+echo "<br>";
+
+$volvo = new volvo("Volvo");
+echo $volvo->description();
+echo "<br>";
+echo $volvo->move();
+echo "<br>";
