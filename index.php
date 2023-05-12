86099bdcdad4535fc4df7408d419f3853b2edcdc
Mohamed Elmanzalawy <mohamed.elmanzalawy98@gmail.com>
2023-05-12 18:09:07 +0300

    Add Interfaces & Polymorphism

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+interface Animal {
+    public function makeSound();
+}
+
+class Cat implements Animal{
+    public function makeSound(){
+        echo 'meow</br>';
+    }
+}
+
+class Cow implements Animal{
+    public function makeSound(){
+        echo 'Moo</br>';
+    }
+}
+
+$cat = new Cat;
+$cow = new Cow;
+
+$cat->makeSound();
+$cow->makeSound();
